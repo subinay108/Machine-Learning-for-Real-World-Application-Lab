3e055ae618ec33765e9b53148183e1fab05a4bca
= <subinayp108@gmail.com>
2025-02-18 20:52:03 +0530

    lab-2 modified

--- Lab-2/linear_regression.py
+++ Lab-2/linear_regression.py
@@ -7,52 +7,49 @@ def mse(actual, predicted):
     loss = sum([(y_i - y_p) ** 2 for y_i, y_p in zip(actual, predicted)]) / n
     return loss
 
-# def 
-
 if __name__=='__main__':
     # Open the dataset
     df = pd.read_csv('data_for_lr.csv')
-    actual_y = df['y']
-    n = len(actual_y)
+    X = df['x']
+    Y = df['y']
+    n = len(X)
     
     m = 0
     b = 0
-    alpha = 0.00001 # Learning rate
+    alpha = 0.0001 # Learning rate
 
     # Before Loss
-    predicted_y = m * df['x'] + b
-    loss = mse(actual_y, predicted_y)
-    print('before loss:', loss)
+    Y_hat = m * X + b
+    loss = mse(Y, Y_hat)
+    print('before training loss:', loss)
     
-    iter = 100
-    for _ in range(iter):   
-        
-        sum_m = 0
-        sum_b = 0
-        for i in range(n):
-            sum_m += (df['y'][i] - (m * df['x'][i] + b)) * df['x'][i]
-            sum_b += (df['y'][i] - (m * df['x'][i] + b))
-            
-        dL_dm = (2 / n) * sum_m 
-        dL_db = (2 / n) * sum_b
-        
-        print('dL_dm, dl_db', dL_dm, dL_db)
-        
+    iter = 10
+    for _ in range(iter): 
+        Y_hat = m * X + b  
+
+        dL_dm = (2 / n) * sum((Y_hat - Y) * X)
+        dL_db = (2 / n) * sum(Y_hat - Y)
+                
         m = m - dL_dm * alpha
         b = b - dL_db * alpha
         
-        print('m, b', m, b)
+        # print('m, b', m, b)
 
     # After loss
-    predicted_y = m * df['x'] + b
-    loss = mse(actual_y, predicted_y)
-    print('After loss:', loss)
+    Y_hat = m * X + b
+    loss = mse(Y, Y_hat)
+    print('After training loss:', loss)
     
-
-
     # Plot the dataset
-    # plt.xlabel('X')
-    # plt.ylabel('Y')
-    # plt.scatter(df['x'], df['y'])
-    # plt.show()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.scatter(X, Y)
+    plt.title('Linear Regression')
+
+    # Draw the regression line
+    line_X = np.linspace(np.min(X), np.max(X), 100)
+    line_Y = line_X * m + b
+    plt.plot(line_X, line_Y, color='red', label=f"y = {m}x + {b}")
+    
+    plt.show()
 
